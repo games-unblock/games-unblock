56c606646e058d9af4ca2d868277da10c9f16e10
abdelrahman <skeletonplayz1@gmail.com>
2025-01-03 19:46:04 -0500

    changed credits

--- games/lava-run/src/credits.js
+++ games/lava-run/src/credits.js
@@ -4,7 +4,7 @@ var creditsState = {
         background.create();
         this.returnButton = game.add.button(64, 64, 'back_button', this.goToMenu, this);
 
-        this.creator = game.add.text(game.world.centerX, 200, 'Code, art & sounds by:', {
+        this.creator = game.add.text(game.world.centerX, 200, 'Code, & art by:', {
             fill: '#424242'
         });
         this.creator.anchor.setTo(0.5, 0.5);
@@ -14,11 +14,11 @@ var creditsState = {
         });
         this.creatorName.anchor.setTo(0.5, 0.5);
 
-        this.musicCreator = game.add.text(game.world.centerX, 400, 'Music by:', {
+        this.musicCreator = game.add.text(game.world.centerX, 400, 'I made this..:', {
             fill: '#424242'
         });
         this.musicCreator.anchor.setTo(0.5, 0.5);
-        this.musicCreatorName = game.add.text(game.world.centerX, 430, 'Oyvind Torvund', {
+        this.musicCreatorName = game.add.text(game.world.centerX, 430, 'Out of boredom', {
             fill: '#fff',
             fontSize: 48
         });
